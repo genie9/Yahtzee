48bdd60c8c4f9e3501c9426216079253ff1e03ba
Evgenia Lyjina <jenny.lyjina@gmail.com>
2023-03-29 00:24:03 +0300

    Adds colors and fonts

--- app/src/main/java/com/example/yahtzee/ui/theme/Color.kt
+++ app/src/main/java/com/example/yahtzee/ui/theme/Color.kt
@@ -2,7 +2,32 @@ package com.example.yahtzee.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+/*
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+*/
+
+//Light Theme example
+val Grey50 = Color(0xFFF8F9FA)
+val Grey900 = Color(0xFF202124)
+val Grey700 = Color(0xFF5F6368)
+val Green50 = Color(0xFFE6F4EA)
+val Green100 = Color(0xFFCEEAD6)
+
+//Light Theme - Woollection https://www.color-hex.com/color-palettes/?keyword=woodsy
+val LightBrown = Color(0xffeee8e9)
+val MidBrown = Color(0xffc7c1b4)
+val DarkBrown = Color(0xff6b564e)
+val LightGreen = Color(0xffd7e5be)
+val LightGrayGreen = Color(0xffeef2eb)
+val GrayGreen = Color(0xffb4bfaf)
+val DarkGreen = Color(0xff9dae91)
+
+//Dark Theme
+val White = Color(0xFFFFFFFF)
+val Grey100 = Color(0xFFF1F3F4)
+val Cyan900 = Color(0xFF007B83)
+val Cyan700 = Color(0xFF129EAF)
+
--- app/src/main/java/com/example/yahtzee/ui/theme/Theme.kt
+++ app/src/main/java/com/example/yahtzee/ui/theme/Theme.kt
@@ -6,31 +6,27 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    background = LightBrown,
+    surface = DarkBrown,
+    onSurface = MidBrown,
+    primary = LightGreen,
+    onPrimary = MidBrown,
+    secondary = DarkGreen
+)
 
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val DarkColorPalette = darkColors(
+    background = DarkBrown,
+    surface = MidBrown,
+    primary = MidBrown,
+    secondary = Grey100
 )
 
 @Composable
 fun YahtzeeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        LightColorPalette
     } else {
         LightColorPalette
     }
