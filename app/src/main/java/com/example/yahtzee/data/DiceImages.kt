8f169b5814226b91e5b9766d4ce343d186fca68e
Evgenia Lyjina <jenny.lyjina@gmail.com>
2023-04-04 10:51:02 +0300

    Adds data files

---
+++ app/src/main/java/com/example/yahtzee/data/DiceImages.kt
@@ -0,0 +1,13 @@
+package com.example.yahtzee.data
+
+import com.example.yahtzee.R
+
+
+val DiceImages = listOf(
+    R.drawable.dice_1,
+    R.drawable.dice_2,
+    R.drawable.dice_3,
+    R.drawable.dice_4,
+    R.drawable.dice_5,
+    R.drawable.dice_6)
+
---
+++ app/src/main/java/com/example/yahtzee/data/ScoreNames.kt
@@ -0,0 +1,28 @@
+package com.example.yahtzee.data
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.example.yahtzee.R
+
+@Composable
+fun ScoreNames(): List<String> {
+    val context = LocalContext.current
+    return listOf(
+        context.resources.getString(R.string.ones),
+        context.resources.getString(R.string.twos),
+        context.resources.getString(R.string.threes),
+        context.resources.getString(R.string.fours),
+        context.resources.getString(R.string.fives),
+        context.resources.getString(R.string.sixes),
+        context.resources.getString(R.string.upper_total),
+        context.resources.getString(R.string.bonus),
+        context.resources.getString(R.string.same_of_three),
+        context.resources.getString(R.string.same_of_four),
+        context.resources.getString(R.string.full_house),
+        context.resources.getString(R.string.small_straight),
+        context.resources.getString(R.string.big_straight),
+        context.resources.getString(R.string.chance),
+        context.resources.getString(R.string.yahtzee),
+        context.resources.getString(R.string.total)
+    )
+}
